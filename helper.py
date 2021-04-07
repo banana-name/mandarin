b17867268dc986ee0632513eba65a53f88ba65da
banana-name <81978290+banana-name@users.noreply.github.com>
2021-04-07 18:26:14 +0300

    Add files via upload

---
+++ helper.py
@@ -0,0 +1,11 @@
+def your_name():
+    name = input("Please, input your name")
+    print("Hello ", name)
+
+def your_age():
+    age = int(input("Please, input your age"))
+    print("OK, your age is: ", age)
+
+def your_hight():
+    hiqht = int(input("Pleasen, input your hiqht"))
+    print("")
